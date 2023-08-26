4cbc5b406310deac51a27bbba57399d3700d92ba
Pis4ni <pis4wrk@gmail.com>
2023-08-26 23:34:58 +0200

    add: milestone 2

--- js/app.js
+++ js/app.js
@@ -1,58 +1,78 @@
 // !recupero gli elementi dal dom
-const slideEl = document.querySelectorAll('slide')
+
 const slidesContainerEl = document.getElementById('slides-container-main')
 const prevEl = document.getElementById('prev')
 const nextEl = document.getElementById('next')
-console.log(slidesContainerEl);
-console.log(slideEl);
+const imagesArray = document.getElementsByClassName('slide')
+
+// ! contatore slide attuale
+let counter = 0
+
+// ! nome classe da aggiungere al dom
 let active = 'active';
  
-
 console.log(images);
 // ! aggiungo dinamicamente le slides al container
 for(const slide of images){
-    console.log('funziona');
-slidesContainerEl.innerHTML += `
-<!-- slides -->
-<div class="slide ${active}">
-    <!-- title -->
-    <h2 class="slide-title">
-        ${slide.title}
-    </h2>
-    <!-- description -->
-    <p class="slide-description">
-        ${slide.text}
-    </p>
-    <!-- source -->
-    <img src="${slide.image}" alt="${slide.text}">
-</div>
-` 
-active = ''
+     
+    slidesContainerEl.innerHTML += `
+    <!-- slides -->
+    <div class="slide ${active}">
+        <!-- title -->
+        <h2 class="slide-title">
+            ${slide.title}
+        </h2>
+        <!-- description -->
+        <p class="slide-description">
+            ${slide.text}
+        </p>
+        <!-- source -->
+        <img src="${slide.image}" alt="${slide.text}">
+    </div>
+    ` 
+    active = ''
 }
 
+//! event listener pulsanti
+prevEl.addEventListener('click', prevSlide)
+nextEl.addEventListener('click', nextSlide)
 
-const imagesArray = document.getElementsByClassName('slide')
-console.log(imagesArray);
-let counter = 0
 
-prevEl.addEventListener('click', ()=>{
-    if (counter >= 0) {
-        counter = imagesArray.legth -1
-    }else{
-        imagesArray[counter].classList.remove('active')
-        counter --
-        imagesArray[counter].classList.add('active')
+//! funzioni ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function nextSlide(){
+
+
+    // rimuovo classe dalla slide attuale
+    imagesArray[counter].classList.remove('active');
+
+    // aumento il contatore
+    counter++; 
+
+    // resetto il contatore se si arriva alla fine
+    if (counter >= imagesArray.length) {
+        counter = 0; 
     }
-})
 
-nextEl.addEventListener('click', ()=>{
-    if (counter == imagesArray.length -1) {
-        counter = 0
-    }else{
-        imagesArray[counter].classList.remove('active')
-        counter ++
-        imagesArray[counter].classList.add('active')
+    // aggiungo classe alla nuova slide attuale
+    imagesArray[counter].classList.add('active'); 
+    console.log(counter);
+}
+
+function prevSlide(){
+    // rimuovo classe dalla slide attuale
+    imagesArray[counter].classList.remove('active');
+
+    // decremento il contatore
+    counter--; 
+
+    // resetto il contatore se si arriva all' inizio
+    if (counter < 0 ) {
+        counter = imagesArray.length -1
     }
-})
 
-        
+    // aggiungo classe alla nuova slide attuale
+    imagesArray[counter].classList.add('active'); 
+    console.log(counter);
+}
+
